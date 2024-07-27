c1900fa1a0fc0d626174e9de4928f499a982eaf4
BrunoAlan <alan.brunoab@gmail.com>
2024-07-26 22:37:25 -0300

    profile store

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,13 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { styles } from '../../../config/app-theme';
 import { useProfileStore } from '../../store/profile-store';
 const ProfileScreen = () => {
   const name = useProfileStore((state) => state.name);
   const email = useProfileStore((state) => state.email);
+  const setName = useProfileStore((state) => state.setName);
+  const setEmail = useProfileStore((state) => state.setEmail);
+  const changeProfile = useProfileStore((state) => state.changeProfile);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{name}</Text>
       <Text style={styles.title}>{email}</Text>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => setName('Alancito')}
+      >
+        <Text>Change name</Text>
+      </Pressable>
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => setEmail('alan.bruno@gmail.com')}
+      >
+        <Text>Change email</Text>
+      </Pressable>
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile('John Doe', 'johndoe@gmail.com')}
+      >
+        <Text>Change profile</Text>
+      </Pressable>
     </View>
   );
 };
--- src/presentation/store/profile-store.ts
+++ src/presentation/store/profile-store.ts
@@ -3,9 +3,15 @@ import { create } from 'zustand';
 export interface ProfileStore {
   name: string;
   email: string;
+  changeProfile: (name: string, email: string) => void;
+  setName: (name: string) => void;
+  setEmail: (email: string) => void;
 }
 
 export const useProfileStore = create<ProfileStore>()((set, get) => ({
   name: 'John Doe',
   email: 'john.doe@google.com',
+  setName: (name: string) => set({ name }),
+  setEmail: (email: string) => set({ email }),
+  changeProfile: (name: string, email: string) => set({ name, email }),
 }));
