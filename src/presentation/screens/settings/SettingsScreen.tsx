44b8fb9e9ca622b82fced6dd32ca385acd695118
BrunoAlan <alan.brunoab@gmail.com>
2024-07-26 22:52:05 -0300

    update settings screen with counter data

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
-import { styles } from "../../../config/app-theme";
+import { Pressable, Text, View } from 'react-native';
+import { styles } from '../../../config/app-theme';
+import { Stack } from 'expo-router';
+import { useCounterStore } from '../../store/counter-store';
 const SettingsScreen = () => {
+  const counter = useCounterStore((state) => state.counter);
+  const incrementBy = useCounterStore((state) => state.incrementBy);
   return (
     <View style={styles.container}>
-      <Text>SettingsSceen</Text>
+      <Stack.Screen
+        options={{
+          headerTitle: `${counter}`,
+        }}
+      />
+      <Pressable style={styles.primaryButton} onPress={() => incrementBy(1)}>
+        <Text>Increment</Text>
+      </Pressable>
+      <Pressable style={styles.primaryButton} onPress={() => incrementBy(-1)}>
+        <Text>Decrement</Text>
+      </Pressable>
     </View>
   );
 };
